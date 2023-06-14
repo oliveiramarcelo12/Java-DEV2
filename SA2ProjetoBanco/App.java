1ec0797e408d1be934234bbee18f487bd9c842e7
Marcelo Oliveira <123783890+oliveiramarcelo12@users.noreply.github.com>
2023-06-14 00:53:48 +0000

    13/06/2023

--- SA2ProjetoBanco/App.java
+++ SA2ProjetoBanco/App.java
@@ -6,25 +6,44 @@ public class App {
     public static void main(String[] args) {
         // criar vetor de objetos
         ContaPJ clientePJ[]= new ContaPJ[10];
+        ContaPF clientePF[]= new ContaPF[10];
         //começar meu banco
         boolean aberto=true;
         int contPJ=0;
+        int contPF=0;
         while (aberto) {
-            int acao = Integer.parseInt(JOptionPane.showInputDialog("Informe a ação Desejada:"    
-                        +"\n 1-Abrir Conta"
-                        +"\n 2-Acessar Conta"
-                        +"\n 3-Sair" ));
+            int acao = Integer.parseInt(JOptionPane.showInputDialog("Informe a ação Desejada:"
+                        +"\n 1-Abrir Conta PJ"
+                        +"\n 2-Abrir Cont PF"
+                        +"\n 3-Acessar Conta PJ"
+                        +"\n 4-Acessar Conta PF"
+                        +"\n 5-Sair"));
             if (acao==1) { //abrir conta
                 //criar um objeto
                 clientePJ[contPJ] = new ContaPJ();
                 clientePJ[contPJ].setNome(JOptionPane.showInputDialog("Informe o Nome da Empresa"));
                 clientePJ[contPJ].setnCnpj(JOptionPane.showInputDialog("Informe o CNPJ da Empresa"));
+                clientePJ[contPJ].setDeposito(0);
                 clientePJ[contPJ].setnConta(2000+contPJ);
                 clientePJ[contPJ].setSaldo(0);
                 //acrescimo no contador
                  JOptionPane.showMessageDialog(null,"Conta Criada com Sucesso");
                 contPJ++;
-            } else if(acao==2){ //acessar uma conta já criada
+            } 
+            else if(acao==2)  {
+                //abrir conta
+                //criar um objeto
+                clientePF[contPF] = new ContaPF();
+                clientePF[contPF].setNome(JOptionPane.showInputDialog("Informe o Nome do Indivíduo"));
+                clientePF[contPF].setnCpf(JOptionPane.showInputDialog("Informe o CPF do Indivíduo"));
+                clientePF[contPF].setnConta(2000+contPF);
+                clientePF[contPF].setSaldo(0);
+                //acrescimo no contador
+                 JOptionPane.showMessageDialog(null,"Conta Criada com Sucesso");
+                contPF++;
+            }
+
+             else if(acao==3){ //acessar uma conta já criada
                 //localizar a conta no vetor
                 int i;//contador
                 int nContaBuscada= Integer.parseInt(JOptionPane.showInputDialog("informe o Número da Conta Buscada"));
@@ -37,12 +56,16 @@ public class App {
                     //menu de acesso da conta
                     boolean acesso = true;
                     while (acesso) {//acessei a conta
-                        int acao2 = Integer.parseInt(JOptionPane.showInputDialog("Informe a operação Desejada:"
+                        int acao3 = Integer.parseInt(JOptionPane.showInputDialog("Informe a operação Desejada:"
                             +"\n 1-Consultar o Saldo"
                             +"\n 2-Realizar um Saque"
-                            +"\n 3-..." ));
-                            if(acao2==1){//saldo
+                            +"\n 3-Realizar um Depósito" ));
+                            if(acao3==1){//saldo
                                 JOptionPane.showMessageDialog(null, "Seu Saldo é de R$ "+clientePJ[i].getSaldo());
+                            }
+                            else if(acao3==2){
+                                JOptionPane.showMessageDialog(null, "Informe Valor do Saque"+clientePJ[i].getSaque());
+
                             }
                     }
                     
@@ -53,9 +76,10 @@ public class App {
 
 
                 
-            }            
+                        
             
         }
     }
+    
    
 }
--- SA2ProjetoBanco/Conta.java
+++ SA2ProjetoBanco/Conta.java
@@ -6,6 +6,9 @@ public abstract class Conta {
     String nome;
     int nConta;
     double saldo;
+    double deposito;
+    double emprestimo;
+    double saque;
     //metódos
     //construtor default(vazio)
     //sets e gets
@@ -28,10 +31,34 @@ public abstract class Conta {
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     } 
+    public double getDeposito() {
+        return deposito;
+    }
+    public void setDeposito(double deposito) {
+        this.deposito = deposito;
+    }
+    public double getEmprestimo() {
+        return emprestimo;
+    }
+    public void setEmprestimo(double emprestimo) {
+        this.emprestimo = emprestimo;
+    }
+    
     //métodos opcionais
     public void saque() {
         double saque = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor do Saque"));
         saldo -=saque;
-    }    
+    } 
+    public void deposito() {
+        double deposito = Integer.parseInt(JOptionPane.showInputDialog("informe o valor do Depósito"));
+        saldo +=deposito;
+        
+    }
+    public double getSaque() {
+        return saque;
+    }
+    public void setSaque(double saque) {
+        this.saque = saque;
+    }   
     
 }
