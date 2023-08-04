83a421f48daa812444196e0273b3f6cb3d5fcebc
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2023-08-03 21:54:41 -0300

    03/08/2023

--- ExemploThrow/ExemploThrow.java
+++ ExemploThrow/ExemploThrow.java
@@ -3,22 +3,39 @@ package ExemploThrow;
 import javax.swing.JOptionPane;
 
 public class ExemploThrow {
-    public static void main(String[] args){
-          // digitação de senhas
-          boolean testesenha = true;
-        while (testesenha) {
-            String senhaDigitada = JOptionPane.showInputDialog("Informe uma Senha de 6 Digitos");
-            try {
-                if (senhaDigitada.length() != 6) {
-                    throw new Exception("Senha Inválida");
-                }
-                testesenha=false;
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, e.getMessage());
-            }
-  }
-
+    public static void main(String[] args) {
+        boolean teste = true;
     
-      
+        while (teste) {
+            String nomeUsuario  =JOptionPane.showInputDialog("Digíte o seu Nome de Usuário");
+            String dataNascimento  =JOptionPane.showInputDialog("Informe o sua data de nascimento [dd/mm/aa]");
+            dataNascimento= dataNascimento.replace("/","");
+            String senha = JOptionPane.showInputDialog("Digite uma Senha de 6 digítos");
+           
+            
+
+            try {
+               
+                if (senha.length() !=6) {
+                    throw new Exception("Senha Inválida");
+                    
+                }//equals comparação
+             if (senha.equals(nomeUsuario) ) {
+                    throw new Exception("Sua senha não pode igual ao Nome de Usário");
+                    
+                }
+                 if(senha.equals(dataNascimento)){
+                    throw new Exception("Sua senha não pode igual a Sua Data de Nascimento");
+
+                }
+                 
+
+               
+                teste=false;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(),null,JOptionPane.CLOSED_OPTION);
+            }
+            
+        }
     }
 }
