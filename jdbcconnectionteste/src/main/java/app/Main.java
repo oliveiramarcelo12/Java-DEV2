ec5a4e48ac7bae005f3c7afad1b51c73d90eb56a
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2023-11-06 21:55:41 -0300

    06/11/2023

---
+++ jdbcconnectionteste/src/main/java/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+import connection.ConnectionDAO;
+
+public class Main {
+    public static void main(String[] args) {
+    new ConnectionDAO().inserir("Marcelo","oliveiramarcelo198@gmail.com");
+    }
+}
