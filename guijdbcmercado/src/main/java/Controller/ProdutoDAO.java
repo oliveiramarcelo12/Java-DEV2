9ea0f659fbb0bf9d7f0e4c56372a40e21cab900d
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2023-12-06 20:26:29 -0300

    06/12/2023

--- guijdbcmercado/src/main/java/Controller/ProdutoDAO.java
+++ guijdbcmercado/src/main/java/Controller/ProdutoDAO.java
@@ -14,14 +14,17 @@ import java.util.List;
 public class ProdutoDAO {
     private Connection connection;
 
+    // Consultas SQL
     private static final String LIMPAR_TABELA = "DELETE FROM produtos";
     private static final String VERIFICAR_REGISTRO = "SELECT COUNT(*) FROM produtos";
 
+    // Inicia uma transação no banco de dados
     public void iniciarTransacao() throws SQLException {
         obterConexao(); // Garante que a conexão está aberta
         connection.setAutoCommit(false);
     }
 
+    // Finaliza uma transação no banco de dados
     public void finalizarTransacao() {
         try {
             connection.commit();
@@ -36,6 +39,7 @@ public class ProdutoDAO {
         }
     }
 
+    // Executa rollback em caso de falha na transação
     public void rollbackTransacao() {
         try {
             if (connection != null) {
@@ -54,11 +58,13 @@ public class ProdutoDAO {
         }
     }
 
+    // Construtor que inicializa a conexão e cria a tabela de produtos se não existir
     public ProdutoDAO() {
         this.connection = ConnectionFactory.getConnection();
         criarTabelaProdutos(); // Chame o método para criar a tabela no construtor, se necessário
     }
 
+    // Cria a tabela de produtos no banco de dados
     private void criarTabelaProdutos() {
         String sql = "CREATE TABLE IF NOT EXISTS produtos (codigo_barra VARCHAR(255), nome VARCHAR(255), quantidade INT, preco DOUBLE)";
         try (Statement stmt = this.connection.createStatement()) {
@@ -69,6 +75,7 @@ public class ProdutoDAO {
         }
     }
 
+    // Adiciona um produto ao banco de dados
     public void adicionarProduto(Produto produto) {
         String sql = "INSERT INTO produtos (codigo_barra, nome, quantidade, preco) VALUES (?, ?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
@@ -80,13 +87,13 @@ public class ProdutoDAO {
             System.out.println("Produto adicionado com sucesso!");
         } catch (SQLException e) {
             System.err.println("Erro ao adicionar produto: " + e.getMessage());
-    
+
             // Em caso de erro, faça o rollback da transação
             rollbackTransacao();
         }
     }
-    
 
+    // Lista todos os produtos do banco de dados
     public List<Produto> listarTodos() {
         PreparedStatement stmt = null;
         ResultSet rs = null;
@@ -112,6 +119,7 @@ public class ProdutoDAO {
         return produtos;
     }
 
+    // Verifica se um registro com o código de barras especificado já existe no banco de dados
     private boolean registroExiste(String codigoBarra) throws SQLException {
         try (PreparedStatement preparedStatement = connection
                 .prepareStatement("SELECT 1 FROM produtos WHERE codigo_barra = ?")) {
@@ -122,10 +130,11 @@ public class ProdutoDAO {
         }
     }
 
+    // Atualiza a tabela do banco de dados com a lista de produtos fornecida
     public void atualizarTabelaBancoDados(List<Produto> produtos) {
         try {
             iniciarTransacao();
-    
+
             // Atualiza apenas os registros existentes e adiciona os novos
             for (Produto produto : produtos) {
                 if (registroExiste(produto.getCodigoBarra())) {
@@ -134,7 +143,7 @@ public class ProdutoDAO {
                     adicionarProduto(produto);
                 }
             }
-    
+
             finalizarTransacao();
         } catch (SQLException e) {
             rollbackTransacao();
@@ -142,8 +151,8 @@ public class ProdutoDAO {
             e.printStackTrace();
         }
     }
-    
 
+    // Atualiza os dados de um produto no banco de dados
     private void atualizarProduto(Produto produto) throws SQLException {
         // Atualiza os dados do produto no banco de dados
         String sql = "UPDATE produtos SET nome=?, quantidade=?, preco=? WHERE codigo_barra=?";
@@ -157,12 +166,14 @@ public class ProdutoDAO {
         }
     }
 
+    // Obtém a conexão, abrindo-a se estiver fechada
     private void obterConexao() throws SQLException {
         if (connection == null || connection.isClosed()) {
             connection = ConnectionFactory.getConnection();
         }
     }
 
+    // Fecha a conexão com o banco de dados
     public void fecharConexao() {
         ConnectionFactory.closeConnection(this.connection);
     }
